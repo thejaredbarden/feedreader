baea8eef4373178bdb36081ecf8b780f89e88ecb
Jared Barden <jdbarden@gmail.com>
2013-03-17 21:43:19 -0400

    Added some starte

---
+++ FeedReader/FeedReader/Models/ArticleModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FeedReader.Models
+{
+    public class ArticleModel
+    {
+        public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Author { get; set; }
+
+        public string Content { get; set; }
+    }
+}
---
+++ FeedReader/FeedReader/Models/FeedModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FeedReader.Models
+{
+    public class FeedModel
+    {
+        public string FeedName { get; set; }
+
+        public string FeedUrl { get; set; }
+
+        public int UnreadCount { get; set; }
+
+        public DateTime LastChecked { get; set; }
+    }
+}
