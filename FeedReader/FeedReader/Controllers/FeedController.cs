14e69594665943f2ee8cf0946aad29039a830dc2
palmern22 <palmern22@gmail.com>
2013-03-18 21:30:42 -0400

    Feed Example
    Added system.servicemodel.syndication and some example code.

---
+++ FeedReader/FeedReader/Controllers/FeedController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace FeedReader.Controllers
+{
+    public class FeedController : Controller
+    {
+        //
+        // GET: /Feed/
+
+        public ActionResult Index()
+        {
+            var feed = SyndicationFeed.Load(XmlReader.Create("http://feeds.dzone.com/dzone/frontpage"));
+            return View(feed);
+        }
+
+    }
+}
